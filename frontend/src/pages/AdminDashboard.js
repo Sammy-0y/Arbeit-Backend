9fcf944bb2cf9eb894993769790315fd31573db0
sanjanashri1805-cmd <sanjana.shri1805@gmail.com>
2026-02-28 20:54:00 +0530

    awdsf

--- frontend/src/pages/AdminDashboard.js
+++ frontend/src/pages/AdminDashboard.js
@@ -5,6 +5,7 @@ import { Card, CardContent, CardHeader, CardTitle } from '../components/ui/card'
 import { Badge } from '../components/ui/badge';
 import { useNavigate } from 'react-router-dom';
 import axios from 'axios';
+import { LayoutDashboard } from "lucide-react";
 import { 
   Users, 
   Briefcase, 
